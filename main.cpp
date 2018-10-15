706336dd571e5867c91f534c5179caf9215bf98c
Bowen <fang131@purdue.edu>
2018-10-15 17:35:45 -0400

    finished reading xml file. still need to figure out how to print xml in string format.

---
+++ main.cpp
@@ -0,0 +1,43 @@
+//
+// Created by bowen fang on 10/15/18.
+//
+#include "main.h"
+
+int main(int argc, char*argv[]){
+    if(argc!=2){
+        cout<<"Game usage: ./Zork filename.xml"<<endl;
+        return 1;
+    }
+
+
+    file<> xmlFile(argv[1]);
+    xml_document<> doc;
+    doc.parse<0>(xmlFile.data());
+    xml_node<>*node=doc.first_node();
+
+    //setup xml lists
+    vector<xml_node<>*> roomxml;
+    vector<xml_node<>*> itemxml;
+    vector<xml_node<>*> containerxml;
+    vector<xml_node<>*> creaturexml;
+
+    for(xml_node<>*top = node->first_node(); top; top=top->next_sibling()){
+        if(string(top->name())==string("room")){
+            roomxml.push_back(top);
+        }
+        if(string(top->name())==string("item")){
+            itemxml.push_back(top);
+        }
+        if(string(top->name())==string("container")){
+            containerxml.push_back(top);
+        }
+        if(string(top->name())==string("creature")){
+            creaturexml.push_back(top);
+        }
+    }
+
+    for(int i =0; i< roomxml.size();i++){
+        cout<<roomxml[i]<<endl;
+    }
+
+}
---
+++ main.h
@@ -0,0 +1,24 @@
+//
+// Created by bowen fang on 10/15/18.
+//
+
+#ifndef ZORK_MAIN_H
+#define ZORK_MAIN_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <list>
+#include <fstream>
+#include <cstdlib>
+#include "rapidxml-1.13/rapidxml.hpp"
+#include "rapidxml-1.13/rapidxml_print.hpp"
+#include "rapidxml-1.13/rapidxml_utils.hpp"
+
+
+using namespace std;
+using namespace rapidxml;
+
+//global variables
+
+#endif //ZORK_MAIN_H
